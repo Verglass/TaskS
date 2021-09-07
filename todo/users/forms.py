ac9d14532c3541276453fce0ec526f2bae06fa91
Verglass <dimonidias@gmail.com>
2021-09-07 17:54:14 +0200

    Initial Commit

---
+++ todo/users/forms.py
@@ -0,0 +1,37 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
+from todo.models import User
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=2, max=20)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirm_password = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign In")
+
+    def validate_name(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                "This username is already taken. Please try another one."
+            )
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                "This email is already taken. Please try another one."
+            )
+
+
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remember = BooleanField("Remember Me")
+    submit = SubmitField("Log In")
---
+++ todo/users/routes.py
@@ -0,0 +1,52 @@
+from flask import Blueprint, render_template, url_for, redirect, flash, request
+from flask_login import current_user, login_user, logout_user, login_required
+from todo import db, bcrypt
+from todo.models import User
+from todo.users.forms import RegistrationForm, LoginForm
+
+users = Blueprint("users", __name__)
+
+
+@users.route("/register", methods=["GET", "POST"])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.home"))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        pw_hash = bcrypt.generate_password_hash(form.password.data).decode("utf-8")
+        user = User(
+            username=form.username.data, email=form.email.data, password=pw_hash
+        )
+        db.session.add(user)
+        db.session.commit()
+        flash(f"Your account has been created! You are now able to log in", "success")
+        return redirect(url_for("users.login"))
+    return render_template("register.html", title="Register", form=form)
+
+
+@users.route("/login", methods=["GET", "POST"])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("main.home"))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get("next")
+            return redirect(next_page) if next_page else redirect(url_for("main.home"))
+        else:
+            flash("Login Unsuccessful. Please check email and password", "danger")
+    return render_template("login.html", title="Login", form=form)
+
+
+@users.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for("main.home"))
+
+
+@users.route("/test")
+@login_required
+def test():
+    return render_template("test.html")
